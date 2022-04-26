6d1c9e5bcdeaddcd71c9aa06a0157b56e939fe4f
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-04-26 13:03:49 -0300

    Implementado o front principal do aplicação

--- src/components/layouts/HeaderComponent.vue
+++ src/components/layouts/HeaderComponent.vue
@@ -1,26 +1,112 @@
 <template>
   <header>
     <div id="data_hoje">
-      <div id="calendar">
-        <img src="svg/calendar.svg" alt="Calendário" />
+      <div class="calendar refeicao" title="Clique para trocar a refeição" @click="alterarRefeicao()">
+        <img src="svg/food.svg" alt="Calendário" />
+        <span>Refeição: {{ refeicaoAtual }}</span>
       </div>
-      <span>21 de abril de 2022 - Café da manhã</span>
+      <div class="calendar">
+        <img src="svg/calendar.svg" alt="Calendário" />
+        <span>{{ dataHoje }}</span>
+      </div>
     </div>
 
     <nav>
-      <img src="svg/menu.svg" alt="Menu" />
-      <div id="nav-itens">
+      <img src="svg/menu.svg" id="btn_menu_ajuda" alt="Menu" @click="menuAtivo = !menuAtivo" />
+      <div id="nav-itens" v-if="menuAtivo">
         <span>Bem vindo, Ramon!</span>
         <a href="#">Ajuda</a>
         <a href="#">Meu perfil</a>
-        <a href="#">Sair</a>
+        <a href="#" class="bg-danger-soft">Sair</a>
       </div>
     </nav>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      refeicaoAtual: '-',
+      menuAtivo: false,
+    };
+  },
+
+  methods: {
+    verificaRefeicaoAtual() {
+      if (localStorage.getItem('refeicaoAtual')) {
+        this.refeicaoAtual = localStorage.getItem('refeicaoAtual');
+      } else {
+        this.refeicaoAtual = 'Café da manhã';
+      }
+    },
+    alterarRefeicao() {
+      if (this.refeicaoAtual == 'Café da manhã') {
+        localStorage.setItem('refeicaoAtual', 'Almoço');
+        this.refeicaoAtual = 'Almoço';
+      } else if (this.refeicaoAtual == 'Almoço') {
+        localStorage.setItem('refeicaoAtual', 'Jantar');
+        this.refeicaoAtual = 'Jantar';
+      } else if (this.refeicaoAtual == 'Jantar') {
+        localStorage.setItem('refeicaoAtual', 'Café da manhã');
+        this.refeicaoAtual = 'Café da manhã';
+      }
+    },
+  },
+  computed: {
+    dataHoje() {
+      let date = new Date();
+      let dia = date.getDate();
+      let mes = '-';
+      let ano = date.getFullYear();
+      switch (date.getMonth()) {
+        case 0:
+          mes = 'Jan';
+          break;
+        case 1:
+          mes = 'Fev';
+          break;
+        case 2:
+          mes = 'Mar';
+          break;
+        case 3:
+          mes = 'Abr';
+          break;
+        case 4:
+          mes = 'Maio';
+          break;
+        case 5:
+          mes = 'Jun';
+          break;
+        case 6:
+          mes = 'Jul';
+          break;
+        case 7:
+          mes = 'Ago';
+          break;
+        case 8:
+          mes = 'Set';
+          break;
+        case 9:
+          mes = 'Out';
+          break;
+        case 10:
+          mes = 'Nov';
+          break;
+        case 11:
+          mes = 'Dez';
+          break;
+
+        default:
+          break;
+      }
+      return `${dia} ${mes} ${ano}`;
+    },
+  },
+  mounted() {
+    this.verificaRefeicaoAtual();
+  },
+};
 </script>
 
 <style></style>
--- src/components/layouts/SidebarComponent.vue
+++ src/components/layouts/SidebarComponent.vue
@@ -13,18 +13,27 @@
         <img src="/svg/user-add.svg" alt="Cadastrar usuário" />
         <span>Cadastrar usuário</span>
       </router-link>
-      <router-link to="/solicitarrefeicao" class="links-item">
-        <img src="/svg/clipboard.svg" alt="Solicitações de refeição" />
-        <span>Solicitar refeição</span>
+      <router-link to="/cadastrarusuario" class="links-item">
+        <img src="/svg/user-edit.svg" alt="Cadastrar usuário" />
+        <span>Gerenciar usuários</span>
       </router-link>
       <router-link to="/registrarpresenca" class="links-item">
         <img src="/svg/user-check.svg" alt="Registrar presença" />
         <span>Registrar presença</span>
       </router-link>
+      <router-link to="/solicitarrefeicao" class="links-item">
+        <img src="/svg/clipboard.svg" alt="Solicitações de refeição" />
+        <span>Solicitar refeição</span>
+      </router-link>
       <router-link to="/visualizarestatisticas" class="links-item">
         <img src="/svg/chart-bar.svg" alt="Estatísticas" />
         <span>Estatísticas</span>
       </router-link>
+    </div>
+    <div id="app_version">
+      <p>Web App <strong>Enumer</strong></p>
+      <p>Todos os direitos reservados © {{ anoAtual }}</p>
+      <p>Versão 1.0.1 | Alpha</p>
     </div>
   </aside>
 </template>
@@ -32,6 +41,11 @@
 <script>
 export default {
   name: 'SidebarComponent',
+  computed: {
+    anoAtual() {
+      return new Date().getFullYear();
+    },
+  },
 };
 </script>
 
