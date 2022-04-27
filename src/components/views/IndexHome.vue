5234dd78031fb54352a40eac0aaa5a013b54736e
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-04-26 23:27:39 -0300

    Implementando CORRETAMENTE o Chart.js

--- src/components/views/IndexHome.vue
+++ src/components/views/IndexHome.vue
@@ -2,16 +2,23 @@
   <PageArea>
     <template v-slot:titulo> Dashboard </template>
     <template v-slot:body>
-      <h3>Bem vindo!</h3>
+      <div id="graficos">
+        <div id="planet">
+          <PlanetChart />
+        </div>
+      </div>
     </template>
   </PageArea>
 </template>
 
 <script>
 import PageArea from '../layouts/PageArea.vue';
+import PlanetChart from '../PlanetChart.vue';
+
 export default {
   components: {
     PageArea,
+    PlanetChart,
   },
 };
 </script>
